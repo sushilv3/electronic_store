f9149f0fdfab1bd76cd31c7d13e5772fe194601f
sushil <sushil.verma@walkingtree.tech>
2023-06-08 13:27:25 +0530

    added validation in Post Api

--- src/main/java/com/sushil/electronic/store/controllers/UserController.java
+++ src/main/java/com/sushil/electronic/store/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/")
-    public ResponseEntity createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity createUser(@RequestBody @Valid UserDto userDto) {
         UserDto user = userService.createUser(userDto);
         return new ResponseEntity(user, HttpStatus.CREATED);
     }

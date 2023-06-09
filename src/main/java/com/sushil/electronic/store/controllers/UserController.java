e61cb570e9492b5ab824c9f810e47da0b207a74e
sushil <sushil.verma@walkingtree.tech>
2023-06-09 16:02:07 +0530

    added user Image upload and serve api

--- src/main/java/com/sushil/electronic/store/controllers/UserController.java
+++ src/main/java/com/sushil/electronic/store/controllers/UserController.java
@@ -1,14 +1,26 @@
 package com.sushil.electronic.store.controllers;
 
 import com.sushil.electronic.store.dtod.ApiResponseMessage;
+import com.sushil.electronic.store.dtod.ImageResponse;
+import com.sushil.electronic.store.dtod.PageableResponse;
 import com.sushil.electronic.store.dtod.UserDto;
+import com.sushil.electronic.store.services.FileService;
 import com.sushil.electronic.store.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -16,6 +28,12 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private FileService fileService;
+    @Value("${user.profile.image.path}")
+    private String imageUploadPath;
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping("/")
     public ResponseEntity createUser(@RequestBody @Valid UserDto userDto) {
@@ -32,13 +50,16 @@ public class UserController {
     @DeleteMapping("/{userId}")
     public ResponseEntity deleteUser(@PathVariable String userId) {
         userService.deleteUser(userId);
-       ApiResponseMessage message= ApiResponseMessage.builder().message("user is deleted successfully ").success(true).status(HttpStatus.OK).build();
-        return new ResponseEntity( message,HttpStatus.OK);
+        ApiResponseMessage message = ApiResponseMessage.builder().message("user is deleted successfully ").success(true).status(HttpStatus.OK).build();
+        return new ResponseEntity(message, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getAllUser() {
-        return new ResponseEntity(userService.getAllUser(), HttpStatus.OK);
+    public ResponseEntity getAllUser(@RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+                                     @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+                                     @RequestParam(value = "sortBy", defaultValue = "name", required = false) String sortBy,
+                                     @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+        return new ResponseEntity(userService.getAllUser(pageNumber, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
 
     @GetMapping("/getUserByUserId/{userId}")
@@ -52,9 +73,34 @@ public class UserController {
         UserDto user = userService.getUserByEmail(email);
         return new ResponseEntity(user, HttpStatus.OK);
     }
+
     @GetMapping("/search/{keyword}")
-    public ResponseEntity getUserByNAme(@PathVariable String keyword) {
-        List<UserDto> users = userService.searchUser(keyword);
+    public ResponseEntity getUserByNAme(@PathVariable String keyword, @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+                                        @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+                                        @RequestParam(value = "sortBy", defaultValue = "name", required = false) String sortBy,
+                                        @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+        PageableResponse<UserDto> users = userService.searchUser(keyword, pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity(users, HttpStatus.OK);
+    }
+
+    //upload user image
+    @PostMapping("/image/{userId}")
+    public ResponseEntity<ImageResponse> uploadUserImage(@PathVariable String userId, @RequestParam("userImage") MultipartFile image) throws IOException {
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+        UserDto userDto = userService.getUserBy(userId);
+        userDto.setImageName(imageName);
+        userService.updateUser(userDto, userId);
+        ImageResponse imageResponse = ImageResponse.builder().imageName(imageName).success(true).status(HttpStatus.CREATED).build();
+        return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
+    }
+
+    //serve user image
+    @GetMapping("/image/{userId}")
+    public void serveUserImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+        UserDto userDto = userService.getUserBy(userId);
+        logger.info("user image name : " + userDto.getImageName());
+        InputStream resource = fileService.getResource(imageUploadPath, userDto.getImageName());
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 }

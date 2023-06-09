221e72438e6b3179bba7005e0640c91aa02db7e7
sushil <sushil.verma@walkingtree.tech>
2023-06-09 16:07:19 +0530

    update pagination

--- src/main/java/com/sushil/electronic/store/repositories/UserRepository.java
+++ src/main/java/com/sushil/electronic/store/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.sushil.electronic.store.repositories;
 
 import com.sushil.electronic.store.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,5 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
-    List<User> findByNameContaining(String keyword);
+    Page<User> findByNameContaining(String keyword, Pageable pageable);
 }

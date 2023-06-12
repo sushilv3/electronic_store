b939d77490235e075fd5ac0c46a88ae6992424ce
sushil <sushilv3rma@gmail.com>
2023-06-12 15:19:44 +0530

    added product repository with custom method

---
+++ src/main/java/com/sushil/electronic/store/repositories/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.sushil.electronic.store.repositories;
+
+import com.sushil.electronic.store.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    Page<Product> findByTitleContaining(String subTitle, Pageable pageable);
+    Page<Product> findByLiveTrue(Pageable pageable);
+}

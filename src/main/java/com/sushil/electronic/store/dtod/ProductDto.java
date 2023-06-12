04b87c0ac2829d9ff859efeb863f6f86c81e153f
sushil <sushilv3rma@gmail.com>
2023-06-12 15:21:28 +0530

    added product dto

---
+++ src/main/java/com/sushil/electronic/store/dtod/ProductDto.java
@@ -0,0 +1,24 @@
+package com.sushil.electronic.store.dtod;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import java.util.Date;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProductDto {
+    private String productId;
+    private String title;
+
+    private String description;
+    private Double price;
+    private Double discountedPrice;
+    private Integer quantity;
+    private Date addedDate;
+    private boolean live;
+    private boolean stock;
+}

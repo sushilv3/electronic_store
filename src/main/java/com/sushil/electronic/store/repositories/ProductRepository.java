b1a6e107d0082aaefad8336967b0ea426f2c647e
sushil <sushilv3rma@gmail.com>
2023-06-13 12:52:59 +0530

    added custom method findByCategory  in repository

--- src/main/java/com/sushil/electronic/store/repositories/ProductRepository.java
+++ src/main/java/com/sushil/electronic/store/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.sushil.electronic.store.repositories;
 
+import com.sushil.electronic.store.entities.Category;
 import com.sushil.electronic.store.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +13,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, String> {
     Page<Product> findByTitleContaining(String subTitle, Pageable pageable);
     Page<Product> findByLiveTrue(Pageable pageable);
+    Page<Product> findByCategory(Category category, Pageable pageable);
+
 }
